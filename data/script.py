f8fe03abef718a7b710fe27746f642c3bd374d79
timothychou <tchou@caltech.edu>
2016-02-11 01:37:46 -0800

    Cleanup script.py

--- data/script.py
+++ data/script.py
@@ -1,9 +1,10 @@
 import os
-
+from tqdm import *
 MAX_ITER = 25
-os.system("cp EmailEnron email_input.txt")
-# os.system("cp EmailEnron email_output.txt")
-for i in range(MAX_ITER):
+#os.system("cp EmailEnron email_input.txt")
+#os.system("cp GNPn100p05  email_input.txt")
+os.system('cp Wiki email_input.txt')
+for i in tqdm(range(MAX_ITER)):
     os.system("python pagerank_map.py < email_input.txt | sort | python "
             "pagerank_reduce.py | python process_map.py | sort | python "
             "process_reduce.py > email_output.txt")
